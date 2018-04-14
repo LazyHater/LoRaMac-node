a3b90487d16773b8ab404fd3bb92320597b6a147
Jamros <wampierek@wp.pl>
2018-04-14 17:18:20 +0200

    Change Set Led

--- src/apps/LoRaMac/classA/LoRaMote/main.c
+++ src/apps/LoRaMac/classA/LoRaMote/main.c
@@ -890,7 +890,7 @@ int main( void )
             }
             case DEVICE_STATE_SEND:
             {
-                GpioWrite(&Led1,1);
+                GpioWrite(&Led1,GpioRead(&Led1) ^ 1);
                 if (GpioRead(&Gpio_Button))
                 {
                     DelayMs(100);
